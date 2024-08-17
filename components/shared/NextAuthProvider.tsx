8c7dd9764729c5595206b8d45f0c219d21a21895
mohammadhoseinrs <mohammadhoseinr1377@gmail.com>
2024-08-17 10:33:09 +0200

    change

--- components/shared/NextAuthProvider.tsx
+++ components/shared/NextAuthProvider.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { Session } from "next-auth";
 import { SessionProvider  } from "next-auth/react";
 
 type NextAuthProviderType={
-    children:React.ReactNode
+    children:React.ReactNode,
+    session?:Session
 }
 const NextAuthProvider = ({ children, session }:NextAuthProviderType) => {
   return <SessionProvider  session={session}>{children}</SessionProvider>;
